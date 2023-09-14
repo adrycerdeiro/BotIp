49c8b6a64fc48dfb3f706727e691dd78653cbf4b
Adrián Cerdeiro Regueira <adrian@barkibu.com>
2023-09-14 20:55:36 +0200

    Block requests if it doesn't come from an allowed user

--- src/bot.js
+++ src/bot.js
@@ -1,12 +1,22 @@
 import axios from "axios";
 import { Telegraf } from "telegraf";
 
+const ALLOWED_USERS = process.env.ALLOWED_USERS.split(',').map((id) => parseInt(id))
+
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.catch((err, ctx) => {
     console.log(`Ooops, encountered an error for ${ctx.updateType}`, err)
     ctx.reply('Something went wrong 🫠');
 });
+
+bot.use(async (ctx, next) => {
+    if (!ALLOWED_USERS.includes(ctx.update.message.from.id)) {
+        ctx.reply('You are not allowed to use this bot!');
+        return;
+    }
+    await next();
+})
 
 bot.start((ctx) => ctx.reply('Welcome! I\'m a bot!'));
 
